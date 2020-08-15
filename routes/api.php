c9e496ade951a25e0499086df7b6d49d7cb4dc3c
Javlonbek Rakhimberdiev <rjavlonbek@Javlonbeks-MacBook-Pro.local>
2020-08-15 14:47:23 +0500

    fix 19

--- routes/api.php
+++ routes/api.php
@@ -613,8 +613,8 @@ Route::post('/mib/lock', function(Request $request){
 	$doc_outgoing_date = $request->doc_outgoing_date;
 	$branch_name = $request->branch_name;
 	$inspector_fio = $request->inspector_fio;
-	$property_pass_info = trim($request->property_pass_info);
-	$property_pass_num = trim($request->property_pass_num);
+	$property_pass_info = $request->property_pass_info ? trim($request->property_pass_info) : "";
+	$property_pass_num = $request->property_pass_num ? trim($request->property_pass_num) : "";
 	$property_number = $request->property_number;
 	$card_number = $request->card_number;
 
